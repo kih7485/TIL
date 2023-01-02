2568a72f35a2f99d67dc28273da64097f8d99b3a
inhan <korea4127@gmail.com>
2023-01-02 23:58:01 +0900

    member test

---
+++ java/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,20 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import study.datajpa.entity.Member;
+
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member){
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
