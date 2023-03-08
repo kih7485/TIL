bd018c9f1055e41f42d4ee03d93d874564237f09
inhan <korea4127@gmail.com>
2023-03-09 00:34:38 +0900

    프론트 컨트롤러 예제

--- java/springboot/helloboot/src/main/java/kih/helloboot/HelloController.java
+++ java/springboot/helloboot/src/main/java/kih/helloboot/HelloController.java
@@ -1,2 +1,13 @@
-package kih.helloboot;public class HelloController {
+package kih.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name){
+        return "hello" + name;
+    }
 }
--- java/springboot/helloboot/src/main/java/kih/helloboot/HellobootApplication.java
+++ java/springboot/helloboot/src/main/java/kih/helloboot/HellobootApplication.java
@@ -1,13 +1,48 @@
 package kih.helloboot;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+	public static void main(String[] args) throws LifecycleException {
+		ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("frontController", new HttpServlet() {
+
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					if(req.getRequestURI().equals("/hello")){
+						String name = req.getParameter("name");
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello Servlet, "+ name);
+
+					}else{
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+				}
+			}).addMapping("/*");
+		});
+		webServer.start();
+
 	}
 
 }
