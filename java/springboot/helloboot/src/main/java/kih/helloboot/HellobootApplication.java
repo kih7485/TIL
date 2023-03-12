6e665e803de49a9c842d7a4e49d5a0c7b8c14cdb
inhan <korea4127@gmail.com>
2023-03-13 01:07:04 +0900

    스프링부트 어플리케이션 직접구현

--- java/springboot/helloboot/src/main/java/kih/helloboot/HellobootApplication.java
+++ java/springboot/helloboot/src/main/java/kih/helloboot/HellobootApplication.java
@@ -1,25 +1,13 @@
 package kih.helloboot;
 
-import org.apache.catalina.LifecycleException;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @ComponentScan
@@ -34,29 +22,7 @@ public class HellobootApplication {
 		return new DispatcherServlet();
 	}
 
-	public static void main(String[] args) throws LifecycleException {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-//		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-				dispatcherServlet.setApplicationContext(this);
-
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-							.addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(HellobootApplication.class);
-		applicationContext.refresh();
-
-
-
+	public static void main(String[] args) {
+		MySpringApplication.run(HellobootApplication.class, args);
 	}
-
 }
