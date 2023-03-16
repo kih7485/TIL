ce04805513fd467cd5d9bab0c44f0179919db507
inhan <korea4127@gmail.com>
2023-03-16 23:48:38 +0900

    빈 구성정보 분리

--- java/springboot/helloboot/src/main/java/config/MySpringBootApplication.java
+++ java/springboot/helloboot/src/main/java/config/MySpringBootApplication.java
@@ -1,7 +1,11 @@
-package kih.helloboot;
+package config;
 
+import config.EnableMyAutoConfiguration;
+import config.autoconfig.DispatcherServletConfig;
+import config.autoconfig.TomcatWebServerConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,5 +16,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
-public @interface MySpringAnnotation {
+@EnableMyAutoConfiguration
+public @interface MySpringBootApplication {
 }
--- java/springboot/helloboot/src/main/java/kih/helloboot/HellobootApplication.java
+++ java/springboot/helloboot/src/main/java/kih/helloboot/HellobootApplication.java
@@ -1,16 +1,9 @@
 package kih.helloboot;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
+import config.MySpringBootApplication;
 
-@MySpringAnnotation
+@MySpringBootApplication
 public class HellobootApplication {
 	public static void main(String[] args) {
 		MySpringApplication.run(HellobootApplication.class, args);
