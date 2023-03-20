d555ca9d9dc6189bd32b52608762fbb6b017fc51
inhan <korea4127@gmail.com>
2023-03-20 23:40:29 +0900

    제티서버 Config 추가

--- java/springboot/helloboot/src/main/java/config/autoconfig/TomcatWebServerConfig.java
+++ java/springboot/helloboot/src/main/java/config/autoconfig/TomcatWebServerConfig.java
@@ -3,13 +3,22 @@ package config.autoconfig;
 import config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatServletWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
+
 }
