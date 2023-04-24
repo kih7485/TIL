09dd2b4a078370b31bd3e000df068afe8f55b290
inhan <korea4127@gmail.com>
2023-04-24 22:52:50 +0900

    프록시 팩토리

--- java/spring/proxy/src/test/java/hello/proxy/cglib/CjlibTest.java
+++ java/spring/proxy/src/test/java/hello/proxy/cglib/CjlibTest.java
@@ -3,6 +3,7 @@ package hello.proxy.cglib;
 import hello.proxy.cglib.code.TimeMethodInterceptor;
 import hello.proxy.common.service.ConcretService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.cglib.proxy.Enhancer;
 
