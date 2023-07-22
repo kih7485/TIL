5edc9b47bd8848809e8c69002e39316c667a423d
inhan <korea4127@gmail.com>
2023-07-23 01:18:38 +0900

    aop 프로젝트 생성

---
+++ java/spring/aop/src/main/java/hello/aop/order/OrderRepository.java
@@ -0,0 +1,13 @@
+package hello.aop.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class OrderRepository {
+        public String save(String itemId){
+            log.info("OrderRepository 실행");
+                return itemId;
+        }
+}
