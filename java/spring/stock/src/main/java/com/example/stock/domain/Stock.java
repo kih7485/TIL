cad2d16d9390d2dff7eeba099ab754d8f03e514e
kih7485 <korea4127@gmail.com>
2023-10-25 21:53:51 +0900

    java 동시성 연습

---
+++ java/spring/stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,46 @@
+package com.example.stock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Stock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+    public Stock(){}
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity){
+        if(this.quantity - quantity < 0){
+            throw  new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+}
