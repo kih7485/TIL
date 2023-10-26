99ddf18112feb2e7b709cd9e4a25205b3522fa7b
kih7485 <korea4127@gmail.com>
2023-10-26 12:43:31 +0900

    redis에서 letuce 사용해보기

--- java/spring/stock/src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
+++ java/spring/stock/src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
@@ -1,4 +1,4 @@
-package com.example.stock.service;
+package com.example.stock.facade;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
@@ -9,17 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class StockServiceTest {
+class LettuceLockStockFacadeTest {
 
     @Autowired
-    private PessimisticLockStockService stockService;
+    private LettuceLockStockFacade lettuceLockStockFacade;
     @Autowired
     private StockRepository stockRepository;
 
@@ -33,15 +32,6 @@ class StockServiceTest {
         stockRepository.deleteAll();
     }
 
-    @Test
-    public void 재고감소(){
-        stockService.decrease(1L,1L);
-
-        //100 - 1 = 99
-        Stock stock = stockRepository.findById(1L).orElseThrow();
-
-        assertEquals(99, stock.getQuantity());
-    }
 
     @Test
     public void 동시에_100개의_요청() throws InterruptedException {
@@ -52,8 +42,10 @@ class StockServiceTest {
         for(int i=0;i<threadCount;i++){
             executorService.submit(() -> {
                 try {
-                    stockService.decrease(1L, 1L);
-                }finally {
+                    lettuceLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
                     latch.countDown();
                 }
 
--- java/spring/stock/src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
+++ java/spring/stock/src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
@@ -1,7 +1,9 @@
-package com.example.stock.service;
+package com.example.stock.facade;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import com.example.stock.service.OptimisticLockStockService;
+import com.example.stock.service.PessimisticLockStockService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,17 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class StockServiceTest {
+class OptimisticLockStockFacadeTest {
 
     @Autowired
-    private PessimisticLockStockService stockService;
+    private OptimisticLockStockFacade optimisticLockStockService;
     @Autowired
     private StockRepository stockRepository;
 
@@ -33,15 +34,6 @@ class StockServiceTest {
         stockRepository.deleteAll();
     }
 
-    @Test
-    public void 재고감소(){
-        stockService.decrease(1L,1L);
-
-        //100 - 1 = 99
-        Stock stock = stockRepository.findById(1L).orElseThrow();
-
-        assertEquals(99, stock.getQuantity());
-    }
 
     @Test
     public void 동시에_100개의_요청() throws InterruptedException {
@@ -52,8 +44,10 @@ class StockServiceTest {
         for(int i=0;i<threadCount;i++){
             executorService.submit(() -> {
                 try {
-                    stockService.decrease(1L, 1L);
-                }finally {
+                    optimisticLockStockService.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
                     latch.countDown();
                 }
 
